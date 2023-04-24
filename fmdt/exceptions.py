d2cb9ae4f2ab9ca1b4acfbe21985838d4129ad7e
archstation <ejovo13@yahoo.com>
2023-04-24 18:19:24 +0200

    add exception

--- fmdt/exceptions.py
+++ fmdt/exceptions.py
@@ -1,3 +1,7 @@
 class GroundTruthError(Exception):
     """Raised when a video doesn't have any ground truths in our data base"""
     pass
+
+class LogError(Exception):
+    """Raised when we have issues with the `log_path` parameter"""
+    pass
