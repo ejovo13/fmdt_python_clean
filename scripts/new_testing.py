68ce19e5a2e5597866c00046cebcc269dd37226a
archstation <ejovo13@yahoo.com>
2023-05-03 14:20:41 +0200

    light_min/light_max

--- scripts/new_testing.py
+++ scripts/new_testing.py
@@ -10,7 +10,7 @@ gt6 = fmdt.load_gt6()
 gt12 = fmdt.load_gt12()
 
 # Far cleaner than before!
-args = fmdt.detect_args(light_min=254, light_max=255, timeout=1)
+args = fmdt.detect_args(ccl_hyst_lo=254, ccl_hyst_hi=255, timeout=1)
 
 success_list = gt6.try_command(args)
 
